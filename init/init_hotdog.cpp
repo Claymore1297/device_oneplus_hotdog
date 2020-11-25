ee156f87cb7f1e3443ecf0ee5f05d00f8cda1a47
Julian Veit <Claymore1298@gmail.com>
2020-11-25 17:29:31 +0100

    hotdog: init: remove some property_override for now
    Change-Id: I9b0ae8fd0f942934cb844f94ff74f51652a27c3b

--- init/init_hotdog.cpp
+++ init/init_hotdog.cpp
@@ -120,19 +120,11 @@ void vendor_load_properties()
             property_override("ro.build.description", "OnePlus7TPro-user 10 QKQ1.190716.003 2009281542 release-keys");
         }
     }
-    property_override("ro.boot.flash.locked", "1");
-    property_override("ro.boot.selinux", "enforcing");
-    property_override("ro.boot.vbmeta.device_state", "locked");
     property_override("ro.boot.verifiedbootstate", "green");
-    property_override("ro.boot.veritymode", "enforcing");
-    property_override("ro.boot.warranty_bit", "0");
     property_override("ro.build.release_type", "release");
-    property_override("ro.build.selinux", "1");
     property_override("ro.build.tags", "release-keys");
     property_override("ro.build.type", "user");
-    property_override("ro.debuggable", "0");
     property_override("ro.secure", "1");
     property_override("ro.vendor.build.release_type", "release");
     property_override("ro.vendor.build.type", "user");
-    property_override("ro.warranty_bit", "0");
 }
